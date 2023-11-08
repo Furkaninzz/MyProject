93e9026a8b00593fbf2b68c33cc42a955eba5469
Furkaninzz <furkanbzby28@gmail.com>
2023-11-08 13:19:39 +0300

    .

--- index.php
+++ index.php
@@ -3,12 +3,23 @@ require_once 'main.php';
 ?>
 <!DOCTYPE html>
 <html lang="tr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?=$_SESSION['page-title']?></title>
-</head>
-<body>
-    
-</body>
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?=$_SESSION['page-title']?></title>
+        <link rel="apple-touch-icon" sizes="180x180" href="assets/favicon/black/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicon/black/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon/black/favicon-16x16.png">
+        <link rel="manifest" href="assets/images/favicon/black/site.webmanifest">
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+        <link rel="stylesheet" href="assets/vendor/bootstrap-5.3.2-dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+
+    <body>
+        <i class="bi-globe2"></i>
+    </body>
+
 </html>
